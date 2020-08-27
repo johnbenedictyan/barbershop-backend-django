22045b5cd80038f83608b76314218b5c7d8e17df
johnbenedictyan <johnyfx00@gmail.com>
2020-08-28 00:11:19 +0800

    Added the urls patterns for the various apps into the project urls file.

--- Barbershop/urls.py
+++ Barbershop/urls.py
@@ -13,9 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# Imports from django
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+# Imports from foreign installed apps
+# from notifications.urls import urlpatterns as notifications_urls
+
+# Imports from local apps
+from barbers.urls import urlpatterns as barbers_urls
+from website.urls import urlpatterns as website_urls
+
+# Start of Urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('barbers/', include(barbers_urls)),
+    path('', include(website_urls)),
 ]
