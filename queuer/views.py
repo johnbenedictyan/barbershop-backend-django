5f3120492195eddc11a6470187d1a2110ced0913
johnbenedictyan <johnyfx00@gmail.com>
2020-08-29 22:08:08 +0800

    Imported uuid4 from uuid and completed the join queue function.

--- queuer/views.py
+++ queuer/views.py
@@ -1,11 +1,47 @@
+# Imports from python
+from uuid import uuid4
+
+# Imports from django
+from django.contrib import messages
 from django.shortcuts import render, redirect
 
-# Create your views here.
+# Imports from foreign installed apps
+
+# Imports from local apps
+from .models import QueueEntry
+
+# Start of Views
 
 
 def JoinQueue(request, barberId):
-    return redirect(reverse('view_queue', barberId))
-
+    try:
+        QueueEntry.objects.get(
+            barberId=barberId
+        )
+    except Exception as err:
+        pass
+    else:
+        try:
+            new_queue_entry = QueueEntry.objects.create(
+                barberId=baberId,
+                uuid=uuid4()
+            )
+        except Exception as err:
+            pass
+        else:
+            return redirect(
+                reverse(
+                    'view_queue',
+                    barberId,
+                    new_queue_entry.id
+                )
+            )
+        return redirect(
+            reverse(
+                'view_queue',
+                barberId
+            )
+        )
 
 def LeaveQueue(request, barberId):
     return redirect(reverse('view_queue', barberId))
