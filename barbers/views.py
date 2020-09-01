0824d046d767ca00a8dbec4e1e66cdc6b6e2ae57
johnbenedictyan <johnyfx00@gmail.com>
2020-09-01 18:43:50 +0800

    Added the query set field so that it will exclude barbers with no queues.

--- barbers/views.py
+++ barbers/views.py
@@ -178,3 +178,11 @@ class BarbershopList(ListView):
     model = User
     template_name = 'barbers.html'
     context_object_name = 'barbers'
+    queryset = User.objects.all().exclude(queue=None)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            
+        })
+        return context
